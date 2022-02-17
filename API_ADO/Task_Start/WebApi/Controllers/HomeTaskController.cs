082a8b568eee8ced16114addc77dcf07440df9e1
yaroslav-ukhan-net <yaroslav.ukhan.net@gmail.com>
2022-02-17 18:18:51 +0200

    branch_1

---
+++ API_ADO/Task_Start/WebApi/Controllers/HomeTaskController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Dto;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeTaskController : ControllerBase
+    {
+        private readonly HomeTaskService homeTaskService;
+
+        public HomeTaskController(HomeTaskService homeTaskService)
+        {
+            this.homeTaskService = homeTaskService;
+        }
+
+
+        // GET: api/<HomeTaskController>
+        [HttpGet]
+        public ActionResult<IEnumerable<HomeTaskDto>> Get()
+        {
+            return Ok(homeTaskService.GetAllHomeTasks().Select(home=>HomeTaskDto.FromModel(home)));
+        }
+
+        // GET api/<HomeTaskController>/5
+        [HttpGet("{id}")]
+        public ActionResult<HomeTaskDto> Get(int id)
+        {
+            var hometask = homeTaskService.GetHomeTaskById(id);
+            if(hometask == null)
+            {
+                return NotFound();
+            }
+            return Ok(HomeTaskDto.FromModel(hometask));
+        }
+
+        // POST api/<HomeTaskController>
+        [HttpPost]
+        public ActionResult Post([FromBody] HomeTaskDto value)
+        {
+            var result = homeTaskService.CreateHomeTask(value.ToModel());
+            if (result.HasErrors)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Accepted();
+
+        }
+
+        // PUT api/<HomeTaskController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] HomeTaskDto value)
+        {
+            var result = homeTaskService.UpdateHomeTask(value.ToModel());
+            if (result.HasErrors)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Accepted();
+        }
+
+        // DELETE api/<HomeTaskController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            homeTaskService.DeleteHomeTask(id);
+            return Accepted();
+        }
+    }
+}
---
+++ API_ADO/Task_Start/WebApi/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ADO.NET;
+using Microsoft.OpenApi.Models;
+using Models;
+using Models.Models;
+using Services;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
+
+            var connectionString = (@"Data Source=DESKTOP-G7US54D\SQLEXPRESS;Initial Catalog=University;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            //my service 
+            services.AddScoped<IRepository<HomeTaskAssessment>>(p => new HomeTaskAssessmentRepository(connectionString));
+            services.AddScoped<IRepository<Student>>(p => new StudentRepository(connectionString));
+            services.AddScoped<IRepository<Course>>(p => new CourseRepository(connectionString));
+            services.AddScoped<IRepository<HomeTask>>(p => new HomeTaskRepository(connectionString));
+            services.AddScoped<StudentService>();
+            services.AddScoped<HomeTaskService>();
+            services.AddScoped<CourseService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "WebApp1 v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
